79efa513da475e7d6710bfdf60aef4d5eab4c97b
taoyuping <835340031@qq.com>
2018-12-07 10:05:21 +0800

    保存application/index/view/index/index.php文件

--- application/index/view/index/index.php
+++ application/index/view/index/index.php
@@ -27,7 +27,6 @@
 					</div>
 				</div>
 			</div>
-			
 			<div class="fr">
 				<div class="newsTap">
 					<div class="slideTxtBox">
@@ -39,7 +38,7 @@
 								{volist name="gglist" id="v"}
 								<li>
 									<span class="date"><{$v.add_time|date='Y-m-d',###}></span>
-									<a href="<{:url('Index/listdetail',['id'=>$v['news_id']])}>" target="_blank"><{$v.title}></a>
+									<a href="<{:url('Index/listdetail',['id'=>$v['news_id']],'html',true)}>" target="_blank"><{$v.title}></a>
 								</li>
 								{/volist}
 								</ul>
@@ -47,7 +46,7 @@
 								{volist name="zxlist" id="v"}
 								<li>
 									<span class="date"><{$v.add_time|date='Y-m-d',###}></span>
-									<a href="<{:url('Index/listdetail',['id'=>$v['news_id']])}>" target="_blank"><{$v.title}></a>
+									<a href="<{:url('Index/listdetail',['id'=>$v['news_id']],'html',true)}>" target="_blank"><{$v.title}></a>
 								</li>
 								{/volist}
 							</ul>
@@ -120,13 +119,13 @@
 					<div class="titBox">
 						<div class="titleBox">
 							<div class="fl tits">国家政策</div>
-							<div class="fr more"><a href="<{:url('Index/newslist',['cate'=>10])}>">更多>></a></div>
+							<div class="fr more"><a href="#">更多>></a></div>
 						</div>
 						<div class="detailBox">
 							<ul>
 								{volist name="gjzclist" id="v"}
 								<li>
-									<a href="<{:url('Index/listdetail',['id'=>$v['news_id']])}>">
+									<a href="<{:url('Index/listdetail',['id'=>$v['news_id']],'html',true)}>">
 										<{$v.title}>
 									</a>
 									<span class="date"><{$v.add_time|date="Y-m-d",###}></span>
@@ -140,13 +139,13 @@
 					<div class="titBox">
 						<div class="titleBox">
 							<div class="fl tits">地方政策</div>
-							<div class="fr more"><a href="<{:url('Index/newslist',['cate'=>11])}>">更多>></a></div>
+							<div class="fr more"><a href="#">更多>></a></div>
 						</div>
 						<div class="detailBox">
 							<ul>
 								{volist name="dfzclist" id="v"}
 								<li>
-									<a href="<{:url('Index/listdetail',['id'=>$v['news_id']])}>">
+									<a href="<{:url('Index/listdetail',['id'=>$v['news_id']],'html',true)}>">
 										<{$v.title}>
 									</a>
 									<span class="date"><{$v.add_time|date="Y-m-d",###}></span>
@@ -229,7 +228,7 @@
 					<div class="titBox">
 						<div class="titleBox">
 							<div class="fl tits">考试信息</div>
-							<div class="fr more"><a href="<{:url('Index/newslist',['cate'=>15])}>">更多>></a></div>
+							<div class="fr more"><a href="#">更多>></a></div>
 						</div>
 						<div class="detailBox">
 							<ul>
@@ -270,13 +269,13 @@
 						<div class="titBox">
 							<div class="titleBox">
 								<div class="fl tits">热门课程</div>
-								<div class="fr more"><a href="<{:url('Index/newslist',['cate'=>21])}>">更多>></a></div>
+								<div class="fr more"><a href="#">更多>></a></div>
 							</div>
 							<div class="detailBox">
 								<ul>
 									{volist name="kclist" id="v"}
 									<li>
-										<a href="<{:url('Index/listdetail',['id'=>$v['news_id']])}>">
+										<a href="<{:url('Index/listdetail',['id'=>$v['news_id']],'html',true)}>">
 											<{$v.title}>
 										</a>
 										<span class="date"><{$v.add_time|date="Y-m-d",###}></span>
@@ -291,13 +290,13 @@
 					<div class="titBox">
 						<div class="titleBox">
 							<div class="fl tits">人才指导</div>
-							<div class="fr more"><a href="<{:url('Index/newslist',['cate'=>22])}>">更多>></a></div>
+							<div class="fr more"><a href="#">更多>></a></div>
 						</div>
 						<div class="detailBox">
 							<ul>
 								{volist name="zdlist" id="v"}
 								<li>
-									<a href="<{:url('Index/listdetail',['id'=>$v['news_id']])}>">
+									<a href="<{:url('Index/listdetail',['id'=>$v['news_id']],'html',true)}>">
 										<{$v.title}>
 									</a>
 									<span class="date"><{$v.add_time|date="Y-m-d",###}></span>
@@ -314,7 +313,7 @@
 					<div class="shizi">
 						<div class="titleBox">
 							<div class="fl tits">师资力量</div>
-							<div class="fr more"><a href="<{:url('Index/newslist',['cate'=>23])}>">更多>></a></div>
+							<div class="fr more"><a href="#">更多>></a></div>
 						</div>
 						<div class="shiziBox">
 							<div class="fl shiziImg">
@@ -372,7 +371,13 @@
 						<ul class="picList">
 							{volist name="comlist" id="v"}
 							<li>
-								<div class="pic"><a href="<{:url('Companys/index',['id'=>$v['company_id']])}>"><img src="<{$v.logo}>"/></a></div>
+								<div class="pic"><a href="#"><img src="<{$v.logo}>"/></a></div>
+								<div class="title"><{$v.company_name}></a></div>
+							</li>
+							{/volist}
+							{volist name="comlist" id="v"}
+							<li>
+								<div class="pic"><a href="#"><img src="<{$v.logo}>"/></a></div>
 								<div class="title"><{$v.company_name}></a></div>
 							</li>
 							{/volist}
